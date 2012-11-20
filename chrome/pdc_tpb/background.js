2dfa1fab5ad0a24cf14d628c05ce2c84ae6a2e6f
sknetrix <sknetrix@hotmail.com>
2012-11-20 01:35:55 +0100

    primera version que habre el magnet
    faltan muchas cosas pero se ha solucionado el problema de la apertura de
    enlaces. era un tema de
    seguridad.http://developer.chrome.com/extensions/contentSecurityPolicy.html

--- chrome/pdc_tpb/background.js
+++ chrome/pdc_tpb/background.js
@@ -1,9 +1,9 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+//codigo de la pagina
 
 // Called when the url of a tab changes.
 function checkForValidUrl(tabId, changeInfo, tab) {
+  //alert(changeInfo.url);
+  console.log(changeInfo.url);
   // If the letter 'g' is found in the tab's URL...
   if (tab.url.indexOf('libro') > -1) {
     // ... show the page action.
@@ -14,5 +14,6 @@ function checkForValidUrl(tabId, changeInfo, tab) {
 
 // Listen for any changes to the URL of any tab.
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
+//chrome.tabs.onUpdated.addListener(function (){console.log(changeInfo.url)})
 chrome.tabs.onActivated.addListener(checkForValidUrl);
 chrome.tabs.onHighlighted.addListener(checkForValidUrl);
--- chrome/pdc_tpb/popup.js
+++ chrome/pdc_tpb/popup.js
@@ -5,29 +5,46 @@
 var req = new XMLHttpRequest();
 req.open(
     "GET",
-	"http://apify.ifc0nfig.com/tpb/search?id=juego de tronos epub",
+	"http://apify.ifc0nfig.com/tpb/search?id=epg",
     true);
 req.onload = muestrabusqueda;
 req.send(null);
 
+console.log("pasada peticion httprequest");
+
+function magnetlink(href){
+	window.open(href);}
 
 function muestrabusqueda() {
   //alert ("c"); //para verficar que se ejecuta esto directamente con tpb no me va?, revisare el XMLHttpRequest porque falla
   var resultados = req.responseText;
   var jobject=JSON.parse(resultados),
      longitud=jobject.length,
-     i,enlaces='';
+     i,enlaces='<p><a href="http://google.com">google</a></p>';
 	 
   for (i=0;i<longitud;i++)
   {
-    enlaces+='<p><a href="'+jobject[i].magnet+'">'+jobject[i].name+'</a></p>';
-	enlaces+='<p>'+jobject[i].magnet+'<p>';
+    enlaces+='<p><a href="'+jobject[i].magnet+'" onclick="window.open(this.href)">'+jobject[i].name+'</a></p>';
+	//enlaces+='<p>'+jobject[i].magnet+'<p>';
+	var p = document.createElement("p");
+	p.innerHTML='<a href="'+jobject[i].magnet+'">'+jobject[i].name+'</a>';
+	document.body.appendChild(p);
+	var en=p.getElementsByTagName("a");
+	console.log(jobject[i].magnet)
+	var magenlace=jobject[i].magnet
+	en[0].addEventListener("click",function () {magnetlink (magenlace)},false)
+	
   }
  //alert(resultados);
-  var p = document.createElement("p");
+  
   //p.innerHTML = resultados[0].innerHTML;
-  document.body.appendChild(p);
-  document.body.innerHTML=enlaces;
+  
+  //document.body.innerHTML=enlaces;
+  /*var enlas=document.getElementsByTagName("a");
+  for (var i = 0; i < enlas.length; i++)
+  {
+	enlas[i].addEventListener("click",magnetlink)
+  }*/
 }
 
-chrome.tabs.onUpdated.addListener(peticion);
+//chrome.tabs.onUpdated.addListener(peticion);
